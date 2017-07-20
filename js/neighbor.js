e82893f7ad698b84033f879a3ea331308c97d1ab
malihat <malihatanweer@gmail.com>
2017-07-20 14:08:01 -0400

    update

--- js/neighbor.js
+++ js/neighbor.js
@@ -9,7 +9,7 @@ var locations = [{
     {
         title: 'New York University',
         location: {
-            lat:  40.7287132,
+            lat: 40.7287132,
             lng: -73.9959727
         }
     },
@@ -30,15 +30,15 @@ var locations = [{
     {
         title: 'New York Law School',
         location: {
-            lat:  40.7180,
-            lng:  -74.0070
+            lat: 40.7180,
+            lng: -74.0070
         }
     },
     {
         title: 'Baruch College',
         location: {
-            lat:  40.7402778,
-            lng:  -73.9844444
+            lat: 40.7402778,
+            lng: -73.9844444
         }
     },
     {
@@ -51,8 +51,8 @@ var locations = [{
     {
         title: 'Marymount Manhattan College',
         location: {
-          lat: 40.7687,
-          lng: -73.9597
+            lat: 40.7687,
+            lng: -73.9597
         }
     },
     {
@@ -65,8 +65,7 @@ var locations = [{
 ];
 
 
-var markers = [];
-var map;
+
 
 //Animates the marker when the item from list or the marker is clicked
 function toggleBounce(marker) {
@@ -77,71 +76,98 @@ function toggleBounce(marker) {
     }
 }
 
+
+
+var markers = [];
+var map;
+var infoWindows, center;
+
 function initMap() {
-        //Add styles to map
-        var styles = [
-        { 
-          featureType: 'water',
-          stylers: [{color: '#76bac1'}]
+    //Add styles to map
+    var styles = [{
+            featureType: 'water',
+            stylers: [{
+                color: '#76bac1'
+            }]
         },
         {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#8f78db'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#f4cca8'}]
-            },
-            {
-               featureType: 'road.highway.controlled_access',
-               elementType: 'geometry',
-               stylers: [{color: '#e2cab5'}]
-            },
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#8f78db'
+            }]
+        },
         {
-          featureType: 'road.local',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#f7a34a'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#bc955e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#c4731d'}]
-            },
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{
+                color: '#17263c'
+            }]
+        },
         {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#994318'}]
-            },
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#f4cca8'
+            }]
+        },
         {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#961803'}]
-            },
+            featureType: 'road.highway.controlled_access',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#e2cab5'
+            }]
+        },
+        {
+            featureType: 'road.local',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#f7a34a'
+            }]
+        },
+        {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{
+                color: '#bc955e'
+            }]
+        },
+        {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#c4731d'
+            }]
+        },
+        {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#994318'
+            }]
+        },
+        {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#961803'
+            }]
+        },
 
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry.fill',
-                stylers: [{color: '#7dc471'}]
-            }
-      ];
+        {
+            featureType: 'poi.park',
+            elementType: 'geometry.fill',
+            stylers: [{
+                color: '#7dc471'
+            }]
+        }
+    ];
 
     //Creates Google map  
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 40.7413549,
-            lng: -73.9980244
+            lat: 40.7831,
+            lng: -73.9712
         },
         zoom: 14,
         styles: styles,
@@ -168,28 +194,21 @@ function initMap() {
         locations[i].marker = marker;
 
         //Create infowindow
-        var infoWindows = new google.maps.InfoWindow();
-        marker.addListener('click', function() {
-            populateInfoWindow(this, infoWindows);
-        });
+        infoWindows = new google.maps.InfoWindow();
+
+        clicked();
+
+        responsive();
 
         bound.extend(marker.position);
-
-        //Makes the map responsive 
-        google.maps.event.addDomListener(window, "resize", function() {
-          var center = map.getCenter();
-          google.maps.event.trigger(map, "resize");
-          map.setCenter(center);
-        });
     }
 
     //Creates infowindow and shows some information about that place from wikipedia when marker is clicked
     function populateInfoWindow(marker, infowindow) {
-
         if (infowindow.marker != marker) {
             //Displays error message if wikipedia doesn't work
             var wikiTimeOut = setTimeout(function() {
-            infowindow.setContent('<div>' + '<strong>' + 'Wikipedia loading error' + '<strong>' + 'Error has occured' + '</div>');
+                infowindow.setContent('<div>' + '<strong>' + 'Wikipedia loading error' + '<strong>' + 'Error has occured' + '</div>');
             }, 5000);
 
             //AJAX request to Wikipedia 
@@ -199,18 +218,31 @@ function initMap() {
                 dataType: "jsonp",
                 success: function(response) {
                     var articleList = response;
-                    infowindow.setContent('<div class="infoWin">' + '<strong>' + marker.title + '</strong>' + '<br>' + articleList[2][0] + '  ' + '<i>' + "(Wikipedia)" + '</i>' + '</div>');                   
+                    infowindow.setContent('<div class="infoWin">' + '<strong>' + marker.title + '</strong>' + '<br>' + articleList[2][0] + '  ' + '<i>' + "(Wikipedia)" + '</i>' + '</div>');
                     infowindow.open(map, marker);
                     clearTimeout(wikiTimeOut);
                 }
             });
-
-
-        }   
-            toggleBounce(marker);
-           
+        }
+        toggleBounce(marker);
     }
     map.fitBounds(bound);
+
+    //Opens infowindow when marker clicked
+    function clicked() {
+        marker.addListener('click', function() {
+            populateInfoWindow(this, infoWindows);
+        });
+    }
+
+    //Makes map responsive
+    function responsive() {
+        google.maps.event.addDomListener(window, "resize", function() {
+            center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+        });
+    }
 }
 
 //Shows error message when google maps does not show.
@@ -238,9 +270,7 @@ var ViewModel = function() {
         self.disappear(!self.disappear());
     };
 
-
-
-    //Opens infowindow on the marker and animates the marker
+    //Opens infowindow on the marker and animates the marker when list item clicked
     self.clicker = function(marker) {
         google.maps.event.trigger(this.marker, 'click');
     };
