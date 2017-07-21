44debcd9478c723a376704384154c344c769c5bc
malihat <malihatanweer@gmail.com>
2017-07-21 19:04:09 -0400

    Separate data

---
+++ js/data.js
@@ -0,0 +1,65 @@
+//Array of all locations
+var locations = [{
+        "title": "Empire State College",
+        "location": {
+            "lat": 40.8075,
+            "lng": -73.9626
+        }
+    },
+    {
+        "title": "New York University",
+        "location": {
+            "lat": 40.7287132,
+            "lng": -73.9959727
+        }
+    },
+    {
+        "title": "Pace University",
+        "location": {
+            "lat": 40.7114914,
+            "lng": -74.0051396
+        }
+    },
+    {
+        "title": "City University of New York",
+        "location": {
+            "lat": 40.8200,
+            "lng": -73.9493
+        }
+    },
+    {
+        "title": "New York Law School",
+        "location": {
+            "lat": 40.7180,
+            "lng": -74.0070
+        }
+    },
+    {
+        "title": "Baruch College",
+        "location": {
+            "lat": 40.7402778,
+            "lng": -73.9844444
+        }
+    },
+    {
+        "title": "Cooper Union",
+        "location": {
+            "lat": 40.7281,
+            "lng": -73.9916
+        }
+    },
+    {
+        "title": "Marymount Manhattan College",
+        "location": {
+            "lat": 40.7687,
+            "lng": -73.9597
+        }
+    },
+    {
+        "title": "Berkeley College",
+        "location": {
+            "lat": 40.7539,
+            "lng": -73.9796
+        }
+    }
+];
